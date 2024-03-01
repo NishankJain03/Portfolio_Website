e1db26e51dfb5ed3d3686ecaed65ddac76153f6b
Nishank Jain <nishankjain122@gmail.com>
2024-03-01 13:04:52 +0530

    Changed Contact Info

--- src/portfolio.js
+++ src/portfolio.js
@@ -139,11 +139,9 @@ const projectSection = {
   display: true // Set false to hide this section, defaults to true
 };
 const contactInfo = {
-  title: emoji("Contact Me ☎️"),
+  title: emoji("Contact Me"),
   subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "nishankjain122@gmail.com"
+    "Reach Out to me!!"
 };
 const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
